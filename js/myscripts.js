296011c29ea88e4f4c2ef4beec3073ab871a8da9
cynthia <cynsellam@gmail.com>
2018-11-15 14:42:10 +0200

    add modal you won

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,7 +1,7 @@
 //The array contains all the images src
 var imagesArray = ["./images/ananas.jpg", "./images/banana.jpg", "./images/citrus.jpg", "./images/kiwi.jpg", "./images/papaye.jpg", "./images/water-melon.jpg", "./images/ananas.jpg", "./images/banana.jpg", "./images/citrus.jpg", "./images/kiwi.jpg", "./images/papaye.jpg", "./images/water-melon.jpg"];
 
-
+var counter = 0;
 
 
 //Var definitions 
@@ -22,19 +22,23 @@ var card12 = document.getElementById("card12");
 
 //var card = document.getElementsByClassName("card");
 //function showImage() {
-   // for (var i = 0; i<card.length; i++) {
-      //  for(var j = 0; j<card[i].length; j++) {
-         //   var x = event.target;
-          //  if(x === card[i]) {
-            //    card[i].src = imagesArray[j]; 
-           // }
-       // }
-   // }
+// for (var i = 0; i<card.length; i++) {
+//  for(var j = 0; j<imagesArray.length; j++) {
+//   var x = event.target;
+//  if(x === card[i]) {
+//    card[i].src = imagesArray[j]; 
+// }
+// }
+// }
 //}
+
+
+
+
 
 function showImage() {
     var x = event.target;
-   if (x.id === "card1") {
+    if (x.id === "card1") {
         card1.src = imagesArray[0];
     } else if (x.id === "card2") {
         card2.src = imagesArray[1];
@@ -73,20 +77,28 @@ function fisherYates() {
 }
 
 //Func new game randomize + returns all the cards
-var newGame = function (){
-    fisherYates(); 
-    back(); 
-    game(); 
+var newGame = function () {
+    fisherYates();
+    back();
+    game();
 }
 
 //Func that returns all the cards back
-var back = function(){
-    for (var i =0; i<card.length; i++){
-        card[i].src = "./images/back.jpg"; 
+var back = function () {
+    for (var i = 0; i < card.length; i++) {
+        card[i].src = "./images/back.jpg";
     }
 }
-var btn = document.getElementById("btn");
-btn.addEventListener("click", newGame);
+var btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", newGame);
+
+//new game modal
+var btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", newGame);
+// replay 
+btn2.onclick = function () {
+    modal.style.display = "none";
+}
 
 //Func that checks if cards are the same
 var openedImg = [];
@@ -97,17 +109,12 @@ var imgOpen = function () {
     if (len === 2) {
         console.log(openedImg[0]);
         console.log(openedImg[1]);
-       // for (var i=0; i<card.length; i++){
-          //  card[i].removeEventListener("click", showImage);
-         //   setTimeout(function(){
-           //     for (var i = 0; i<card.length; i++){
-            //        card[i].addEventListener("click", showImage);
-            //    }
-          //  }, 1000); 
-       // }
+        disableGame();
         if (openedImg[0].src === openedImg[1].src) {
             console.log("same");
             matched();
+
+
         } else {
             console.log("different");
             unmatched();
@@ -120,6 +127,7 @@ var matched = function () {
     openedImg[0].removeEventListener("click", showImage);
     openedImg[1].removeEventListener("click", showImage);
     openedImg = [];
+    scoreCounter();
 }
 
 
@@ -130,7 +138,7 @@ var unmatched = function () {
     interval = setTimeout(function () {
         openedImg[0].src = "./images/back.jpg";
         openedImg[1].src = "./images/back.jpg";
-        openedImg = []; 
+        openedImg = [];
     }, 1000);
 }
 
@@ -143,3 +151,32 @@ game = function () {
         card[i].addEventListener("click", imgOpen);
     }
 }
+
+//disable for a second
+disableGame = function () {
+    for (var i = 0; i < card.length; i++) {
+        card[i].removeEventListener("click", showImage);
+        card[i].removeEventListener("click", imgOpen);
+    }
+    setTimeout(function () {
+        game()
+    }, 1000);
+}
+
+//display you won 
+var modal = document.getElementById("display-score");
+scoreCounter = function () {
+    console.log(counter = counter + 1);
+    if (counter === 6) {
+        console.log("you won");
+        // get the modal
+        var span = document.getElementsByClassName("close")[0]; // get the span that closes the modal
+        modal.style.display = "block";
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+    }
+}
+
+
+
